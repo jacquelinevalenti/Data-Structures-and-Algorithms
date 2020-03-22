e53bbd7cd19e173f6a449db86b9446d67b889c14
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-03-22 12:12:03 -0700

    finish onechangeaway problem

--- OneChangeAwayStrings/OneChangeAwayStrings/Program.cs
+++ OneChangeAwayStrings/OneChangeAwayStrings/Program.cs
@@ -6,16 +6,16 @@ namespace OneChangeAwayStrings
     {
         static void Main(string[] args)
         {
-            isOneAway("abcde", "abcd");  // should return true
-            isOneAway("abde", "abcde");  // should return true
-            isOneAway("a", "a");  // should return true
-            isOneAway("abcdef", "abqdef");  // should return true
-            isOneAway("abcdef", "abccef");  // should return true
-            isOneAway("abcdef", "abcde");  // should return true
-            isOneAway("aaa", "abc");  // should return false
-            isOneAway("abcde", "abc");  // should return false
-            isOneAway("abc", "abcde");  // should return false
-            isOneAway("abc", "bcc");  // should return false
+            Console.WriteLine(IsOneAway("abcde", "abcd"));  // should return true
+            Console.WriteLine(IsOneAway("abde", "abcde"));  // should return true
+            Console.WriteLine(IsOneAway("a", "a"));  // should return true
+            Console.WriteLine(IsOneAway("abcdef", "abqdef"));  // should return true
+            Console.WriteLine(IsOneAway("abcdef", "abccef"));  // should return true
+            Console.WriteLine(IsOneAway("abcdef", "abcde"));  // should return true
+            Console.WriteLine(IsOneAway("aaa", "abc"));  // should return false
+            Console.WriteLine(IsOneAway("abcde", "abc"));  // should return false
+            Console.WriteLine(IsOneAway("abc", "abcde"));  // should return false
+            Console.WriteLine(IsOneAway("abc", "bcc"));  // should return false
         }
 
         // My solution
@@ -35,48 +35,86 @@ namespace OneChangeAwayStrings
             // 4a. if this comparison is true at the end, return true
             // 4b. if this comparison fails at any point, break and return false
 
-        public static bool isOneAway(string s1, string s2)
+        public static bool IsOneAway(string s1, string s2)
         {
             // check if two strings are the same
-            // first if statement is the shortest way, though this may be a constraint in an interview
+            // first if statement is the shortest way to check if strings are exactly the same, though this may be a constraint in an interview
+            // third if statement will cover this condition if we get rid of the .equals
 
             //if (s1.Equals(s2))
             //{
             //    return true;
             //}
 
-            if (s1.Length == s2.Length)
-            {
-                for (int i = 0; i < s1.Length; i++)
-                {
-                    if (s1[i] != s2[i])
-                    {
-                        break;
-                    }
-                    
-                  
-
-                }
-            }
-
             // check if difference between string sizes is more than 1; if it is, return false
-            // could use absolute value math function instead of or statement; I'm assuming this is a constraint
-            else if (s1.Length - s2.Length > 1 || s2.Length - s1.Length > 1)
+            // could use absolute value math function instead of ||; I'm assuming this is a constraint
+            if (s1.Length - s2.Length > 1 || s2.Length - s1.Length > 1)
             {
                 return false;
             }
 
-            // if lengths are within 1 of each other, try to make them the same string
+            // if lengths are the same, check to see how many chars are different between the two strings with SameLength function
+            else if (s1.Length == s2.Length)
+            {
+                return SameLength(s1, s2);
+            }
+
+            // if lengths are within one of each other, call DiffLength function
+            // if s1 is longer, put s1 first in parameters
+            // if s2 is longer, put s2 first in parameters
+            else if (s1.Length > s2.Length)
+            {
+                return DiffLengths(s1, s2);
+            }
             else
             {
-
+                return DiffLengths(s2, s1);
             }
-            
-            
+        }
+
+        public static bool SameLength(string s1, string s2)
+        {
+            int wrongCount = 0;
+
+            // loop through the array and find where they don't match
+            for (int i = 0; i < s1.Length; i++)
+            {
+                // count how many chars are not equal in the two strings
+                if (s1[i] != s2[i])
+                {
+                    wrongCount++;
+                }
+                if (wrongCount > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool DiffLengths(string s1, string s2)
+        {
+            int missingCount = 0;
+            int i = 0;
+            while (i < s2.Length)
+            {
+                if (s1[i + missingCount] == s2[i])
+                {
+                    i++;
+                }
+                else
+                {
+                    missingCount++;
+                    if (missingCount > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
     }
 
-  
 }
 
