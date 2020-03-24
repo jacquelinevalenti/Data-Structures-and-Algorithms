14ed3892cd1502c170d4da8d341bd793d3cd52ec
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-03-23 17:18:13 -0700

    addition of minesweeper problem

---
+++ MineSweeper/MineSweeper/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] bombs1 = { { 0, 2 }, { 2, 0 } };
+            // mineSweeper(bombs1, 3, 3) should return:
+            // [[0, 1, -1],
+            //  [1, 2, 1],
+            //  [-1, 1, 0]]
+
+            int[,] bombs2 = { { 0, 0 }, { 0, 1 }, { 1, 2 } };
+            // mineSweeper(bombs2, 3, 4) should return:
+            // [[-1, -1, 2, 1],
+            //  [2, 3, -1, 1],
+            //  [0, 1, 1, 1]]
+
+            int[,] bombs3 = { { 1, 1 }, { 1, 2 }, { 2, 2 }, { 4, 3 } };
+            // mineSweeper(bombs3, 5, 5) should return:
+            // [[1, 2, 2, 1, 0],
+            //  [1, -1, -1, 2, 0],
+            //  [1, 3, -1, 2, 0],
+            //  [0, 1, 2, 2, 1],
+            //  [0, 0, 1, -1, 1]]
+
+            int[,] result1 = MineSweeper(bombs1, 3, 3);
+            int[,] result2 = MineSweeper(bombs2, 3, 4);
+            int[,] result3 = MineSweeper(bombs3, 5, 5);
+            Print2DArray(result1);
+            Console.WriteLine();
+            Print2DArray(result2);
+            Console.WriteLine();
+            Print2DArray(result3);
+            Console.WriteLine();
+
+        }
+
+        // no duplicates in bombs
+        // bombs represented as -1
+        // surrounding numbers have to indicate how many bombs are around that location
+        public static int[,] MineSweeper(int[,] bombs, int numRows, int numCols)
+        {
+            // create the resulting 2D array with size numRows x numCols
+            int[,] field = new int[numRows, numCols];
+            // initialize bomb locations in field
+            for (int b = 0; b < (bombs.Length / 2); b++)
+            {
+                int rowIndex = bombs[b, 0];
+                int colIndex = bombs[b, 1];
+                // bomb located at row i and column i
+                field[rowIndex, colIndex] = -1;
+
+                // outside loop iterates from row i - 1 to row i + 1
+                // inside loop iterates from column i - 1 to column i + 1
+                for (int i = rowIndex - 1; i < rowIndex + 2; i++)
+                {
+                    for (int j = colIndex - 1; j < colIndex + 2; j++)
+                    {
+                        if (0 <= i && i < numRows && 0 <= j && j < numCols && field[i,j] != -1)
+                        {
+                            // add 1 to surrounding squares (9 surrounding cells)
+                            field[i,j] += 1;
+                        }
+                    }
+                }
+            }
+            return field;
+        }
+
+
+        // method to print out the array in a nice visual way
+        public static void Print2DArray(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"[{matrix[i, j]}]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
