f00cc40af17419d1efecb78e328161fa239e9b9c
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-03-25 16:18:59 -0700

    addition of string reversal problem

---
+++ ReverseStrings/ReverseStrings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string to be reversed: ");
+            string input = Console.ReadLine();
+            string reversed = ReverseString(input);
+            Console.WriteLine(reversed);
+
+        }
+
+        // write a method that takes an array of characters and reverses the letters in place
+        // manipulate the strings in place (modify the input; don't make a copy)
+        // INPUT: string
+        // OUTPUT: reversed string
+        // ASSUMPTIONS: string can be any length
+        // CONSTRAINTS: don't use reverse
+        // steps:
+        // 1. convert the input to a char array
+        // 2. add each char to a stack (since we want to reverse it in place)
+        // 3. pop each char off the stack and return
+
+        public static string ReverseString(string s)
+        {
+            char[] reversedarray = new char[s.Length];
+
+            Stack stack = new Stack();
+            foreach (char c in s)
+            {
+                stack.Push(c);
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                char rev = (char)stack.Pop();
+                reversedarray[i] = rev;
+            }
+            string reversed = new string(reversedarray);
+            return reversed;
+           
+        }
+    }
+}
