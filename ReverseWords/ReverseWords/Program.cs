ed62ed1c74363098628d92a409e0a251946c6985
Jackie Valenti <jacquelinevalenti@outlook.com>
2020-03-26 12:20:22 -0700

    another method for reversewords

--- ReverseWords/ReverseWords/Program.cs
+++ ReverseWords/ReverseWords/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReverseWords
 {
@@ -7,11 +8,14 @@ namespace ReverseWords
         static void Main(string[] args)
         {
             char[] arr = { 'i', 's', ' ', 'n', 'a', 'm', 'e', ' ', 'm', 'y' };
-            ReverseWords(arr);
-            foreach (char c in arr)
-            {
-                Console.Write(c);
-            }
+            //ReverseWords(arr);
+            //foreach (char c in arr)
+            //{
+            //    Console.Write(c);
+            //}
+
+            Console.WriteLine(ReverseAgain(arr));
+
         }
 
         // given a char array of words, reverse the words (not the letters)
@@ -23,6 +27,39 @@ namespace ReverseWords
         // 2. stop when you find a space
         // 3. repeat that process through the array
         // 4. convert to a string and print
+
+
+        public static string ReverseAgain(char[] arr)
+        {
+            string result = string.Empty;
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i <= arr.Length; i++)
+            {
+                if (i == arr.Length)
+                {
+                    stack.Push(result);
+                    result = string.Empty;
+                }
+                else if (arr[i] == ' ')
+                {
+                    result = string.Concat(result, arr[i]);
+                    stack.Push(result);
+                    result = string.Empty;
+                }
+                else
+                {
+                    result = string.Concat(result, arr[i]);
+                }
+            }
+            {
+                
+            }
+            foreach (var item in stack)
+            {
+                result += string.Concat(item);
+            }
+            return result;
+        }
 
         public static void ReverseWords(char[] arr)
         {
